6e1bcbea858303a3c38c27c4aad061c7c21ac354
dpa975 <129889645+dpa975@users.noreply.github.com>
2023-06-28 22:35:10 +0000

    2nd Update

--- Scripts/query.sql
+++ Scripts/query.sql
@@ -3,7 +3,7 @@ mng.firstName AS ManagerFirstName,
 mng.lastName AS ManagerLastName
 FROM employee emp
 INNER JOIN employee mng
-ON emp.managerID = mng.employeeID
+ON emp.managerID = mng.employeeID;
 
 GO 
 
@@ -12,4 +12,47 @@ FROM employee
 LEFT JOIN sales
 ON employee.employeeId = sales.employeeId
 WHERE title = 'Sales Person'
-AND salesAmount IS NULL
+AND salesAmount IS NULL;
+
+GO 
+
+SELECT emp.firstName, emp.lastName, emp.employeeId, 
+COUNT(*) AS NumOfCarsSold
+FROM employee emp
+INNER JOIN sales sls
+ON emp.employeeId = sls.employeeId
+GROUP BY 1,2,3
+ORDER BY 4 DESC;
+
+GO 
+
+SELECT emp.employeeId, emp.firstName, emp.lastName,
+MIN(salesAmount) AS MinSalesAmount,
+MAX(salesAmount) AS MaxSalesAmount
+FROM sales sls
+INNER JOIN employee emp 
+ON sls.employeeId = emp.employeeId
+WHERE sls.soldDate >= date('now', 'start of year')
+GROUP BY 1,2,3
+ORDER BY 5 DESC;
+
+GO 
+
+WITH cte AS (
+SELECT strftime('%Y', soldDate) AS soldYear, salesAmount
+FROM sales
+)
+SELECT soldYear, FORMAT("$%.2f", sum(salesAmount)) AS annualSales
+FROM cte 
+GROUP BY soldYear
+ORDER BY soldYear;
+
+GO 
+
+SELECT emp.firstName, emp.lastName, sls.soldDate, sls.salesAmount
+FROM sales sls 
+INNER JOIN employee emp 
+ON sls.employeeId = emp.employeeId
+WHERE sls.soldDate >= '2021-01-01'
+AND sls.soldDate <= '2022-01-01'
+ORDER BY soldDate ASC;
